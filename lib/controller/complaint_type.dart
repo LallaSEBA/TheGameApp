8e93677ef0e16119298463cba2e158ddc65c642e
MONSEFOT <abdoumanou5@gmail.com>
2021-01-13 16:55:20 +0100

    complaint_controller

---
+++ lib/controller/complaint_type.dart
@@ -0,0 +1,7 @@
+enum ComplaintType { complaint, thanks, suggestion }
+
+var complaintTypeString = <ComplaintType , String>{
+  ComplaintType.complaint : "complaint",
+  ComplaintType.thanks : "thanks",
+  ComplaintType.suggestion : "suggestion",
+}; 
